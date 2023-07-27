34e1936a3c02a25709e60982837af07d279ec665
Moloto42 <mokgadibeauty@gmail.com>
2023-07-26 18:36:31 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,11 +17,11 @@ int length_of_string, z;
 length_of_string = 0;
 while (dest[length_of_string] != '\0')
 {
-		length_of_string++;
+length_of_string++;
 }
 for (z = 0; z < n && src[z] != '\0'; z++, length_of_string++)
 {
-		dest[length_of_string] = src[z];
+dest[length_of_string] = src[z];
 }
 dest[length_of_string] = '\0';
 return (dest);
